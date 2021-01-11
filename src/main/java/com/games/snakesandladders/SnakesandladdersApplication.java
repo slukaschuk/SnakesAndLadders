612232dbdc8396c9d6d01e7ca0c53aadbdaa2e3c
serhii.lukashchuk <serhii.lukashchuk@ligazakon.ua>
2021-01-11 18:00:19 +0200

    Initial commit

---
+++ src/main/java/com/games/snakesandladders/SnakesandladdersApplication.java
@@ -0,0 +1,13 @@
+package com.games.snakesandladders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakesandladdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakesandladdersApplication.class, args);
+	}
+
+}
