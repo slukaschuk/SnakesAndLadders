3de908666696a539470dba7e8a2ea497e386accc
Serhii_Lukashchuk <serhii_lukashchuk@epam.com>
2021-01-13 12:33:49 +0200

    Moves Are Determined By Dice Rolls - Refactoring.Changing the player's behavior model.

--- src/main/java/com/games/snakesandladders/models/Player.java
+++ src/main/java/com/games/snakesandladders/models/Player.java
@@ -1,13 +1,8 @@
 package com.games.snakesandladders.models;
 
-import com.games.snakesandladders.services.Dice;
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class Player {
 
     private Token token;
-
-    private Dice dice;
 
     public Token getToken() {
         return token;
@@ -17,17 +12,9 @@ public class Player {
         this.token = token;
     }
 
-    @Autowired
-    public void setDice(Dice dice) {
-        this.dice = dice;
-    }
-
     public void moveToken(int spaces) {
         int currentPosition = token.getPosition();
         token.setPosition(currentPosition + spaces);
     }
 
-    public int roll() {
-        return dice.roll();
-    }
 }
